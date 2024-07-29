9352298f99b74ef48631146c0ac77da00ddc7b1a
jashandeep <jashandeep-singh13@myseneca.ca>
2024-07-28 20:33:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /********************************************************************************
-* WEB322 – Assignment 04
+* WEB322 – Assignment 05
 * 
 * I declare that this assignment is my own work in accordance with Seneca's
 * Academic Integrity Policy:
 * 
 * https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 * 
-* Name: Jashandeep Singh   Student ID: 145936225   Date: 14/07/2024
+* Name: Jashandeep Singh   Student ID: 145936225   Date: 28/07/2024
 *
 * Published URL: https://ass4-eight.vercel.app/
 *
